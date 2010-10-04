afe236d39067678deb683c14ccff9462611cb547
martin <martin@pythonic.eu>
2010-10-04 13:48:58 +0200

    Added admin for models

---
+++ main/admin.py
@@ -0,0 +1,11 @@
+from mattespill.main.models import Group, User, Task, Room, Result, Answer
+
+from django.contrib import admin
+
+admin.site.register(Group)
+admin.site.register(User)
+admin.site.register(Task)
+admin.site.register(Room)
+admin.site.register(Result)
+admin.site.register(Answer)
+
