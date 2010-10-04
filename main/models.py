77a7fed66ce868f19025f6183606fad9b662fded
martin <martin@pythonic.eu>
2010-10-04 11:42:34 +0200

    Fixed models

--- main/models.py
+++ main/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 
-class Task(model.Model):
+class Task(models.Model):
     question = models.CharField(max_length=200)
     date = models.DateTimeField('date')
     answer = models.CharField(max_length=100)
 
-class User(model.Model):
+class User(models.Model):
     access = models.IntegerField()
     username = models.CharField(max_length=40)
     password = models.CharField(max_length=32) # MD5 hash
+
 
