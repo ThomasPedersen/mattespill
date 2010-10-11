931492a2fb2d837af6680600843f1f618e4561be
vegardoj <vegard-2@online.no>
2010-10-11 10:37:10 +0200

    changes to UserProfile

--- main/models.py
+++ main/models.py
@@ -1,10 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
 
-class UserProfile(models.Model):
+class UserProfile(models.Model):    
+    user = models.ForeignKey(User, unique=True)
     points = models.IntegerField()
-    user = models.ForeignKey(User, unique=True)    
+    
+    def __str__(self):
+        return "%s's profile" % self.user
+    
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        profile, created = UserProfile.objects.get_or_create(user=instance)
+        
+post_save.connect(create_user_profile, sender=User)
     
 
 class Room(models.Model):
