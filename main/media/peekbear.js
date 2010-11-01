11aba6235df983e5df93ebb02bf7b85f90ca13dd
Mikael Finstad <finstaden@gmail.com>
2010-11-01 14:04:08 +0100

    pnghelvete

--- main/media/peekbear.js
+++ main/media/peekbear.js
@@ -1,47 +1,51 @@
 var peekbearImages = ['peekbearbottom.png', 'peekbearright.png', 'peekbearleft.png'];
 
 function showPeekbear() {
-	var i = Math.floor(Math.random()*(peekbearImages.length+1));
+	var i = Math.floor(Math.random()*(peekbearImages.length));
 	var element = $('<img src="/media/images/' + peekbearImages[i] + '" alt="Tittei" />').
 		css('position', 'fixed');
 
+	var bearSize = '3em';
+
 	switch (i) {
 		case 0:
-			element.css('height', '5em').css('bottom', '-5em').
+			element.css('height', bearSize).css('bottom', '-' + bearSize).
 			css('left', Math.floor(5 + Math.random()*(80-5)) + '%').animate({
 				bottom: '0em'
 			}, 2000, function() {
 				$(this).animate({
-					bottom: '-5em'
+					bottom: '-' + bearSize
 				}, 300, function() {
 					$('body').remove(this);
 				});
 			});
 			break;
 		case 1:
-			element.css('width', '5em').css('right', '-5em').
-			css('top', Math.floor(5 + Math.random()*(80-5)) + '%').animate({
+			element.css('width', bearSize).css('right', '-' + bearSize).
+			css('bottom', Math.floor(5 + Math.random()*(80-5)) + '%').animate({
 				right: '0em'
 			}, 2000, function() {
 				$(this).animate({
-					right: '-5em'
+					right: '-' + bearSize
 				}, 300, function() {
 					$('body').remove(this);
 				});
 			});
 			break;
 		case 2:
-			element.css('width', '5em').css('left', '-5em').
-			css('top', Math.floor(5 + Math.random()*(80-5)) + '%').animate({
+			element.css('width', bearSize).css('left', '-' + bearSize).
+			css('bottom', Math.floor(5 + Math.random()*(80-5)) + '%').animate({
 				left: '0em'
 			}, 2000, function() {
 				$(this).animate({
-					left: '-5em'
+					left: '-' + bearSize
 				}, 300, function() {
 					$('body').remove(this);
 				});
 			});
 			break;
+		default:
+			return;
 	}
 
 	$('body').append(element);
